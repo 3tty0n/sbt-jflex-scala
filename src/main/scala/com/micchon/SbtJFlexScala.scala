8683c457f9c40b6b100635a22f43de06ba0ce032
Yusuke Izawa <yuizalp@gmail.com>
2017-01-05 17:38:47 +0900

    update SbtJflexScala.scala and test

--- src/main/scala/com/micchon/SbtJFlexScala.scala
+++ src/main/scala/com/micchon/SbtJFlexScala.scala
@@ -7,30 +7,68 @@ import jflex.{ Options, Main }
 
 object SbtJFlexScala extends Plugin {
 
+  case class JFlexToolConfiguration(
+    dot: Boolean = false,
+    dump: Boolean = false,
+    verbose: Boolean = false,
+    emitScala: Boolean = true
+  )
+
+  case class PluginConfiguration(
+    grammarSuffix: String = ".flex"
+  )
+
   lazy val jflexSourceDirectory = settingKey[File]("jflex-source-directory")
   lazy val jflexOutputDirectory = settingKey[File]("jflex-output-directory")
+  lazy val toolConfiguration = settingKey[JFlexToolConfiguration]("jflex-tool-configuration")
+  lazy val pluginConfiguration = settingKey[PluginConfiguration]("jflex-plugin-configuration")
   lazy val jflexSources = taskKey[Seq[File]]("jflex-sources")
   lazy val jflexGenerate = taskKey[Unit]("jflex-generate")
 
   override lazy val settings: Seq[Setting[_]] = Seq(
     jflexSourceDirectory := sourceDirectory.value / "main" / "flex",
-    jflexOutputDirectory := sourceDirectory.value / "main" / "scala",
-    jflexSources <<= jflexSourceDirectory map { dir =>
-      (dir ** "*.flex").get
-    },
-    jflexGenerate <<= jflexGenerateTask
+    jflexOutputDirectory := sourceDirectory.value / "main" / "scala" / "flex",
+    toolConfiguration := JFlexToolConfiguration(),
+    pluginConfiguration := PluginConfiguration(),
+    jflexSources := (jflexSourceDirectory.value ** "*.flex").get,
+    jflexGenerate := jflexGenratorTask.value,
+    unmanagedSourceDirectories in Compile += jflexSourceDirectory.value
   )
 
-  def jflexGenerateTask = (jflexSources, jflexOutputDirectory, streams) map {
-    (src, dir, s) => {
-      Options.dot = false
-      Options.dump = false
-      Options.verbose = false
-      Options.emitScala = true
-      Options.setDir(dir.getPath)
-      s.log.info("src: " + src)
-      src.foreach(s => Main.generate(s))
+  lazy val jflexGenratorTask = Def.task {
+    generateWithJFlex(
+      jflexSources.value,
+      jflexOutputDirectory.value,
+      toolConfiguration.value,
+      pluginConfiguration.value,
+      streams.value.log
+    )
+  }
+
+  private[this] def generateWithJFlex(
+    srcDir: Seq[File],
+    target: File,
+    tool: JFlexToolConfiguration,
+    options: PluginConfiguration,
+    log: Logger
+  ): Unit = {
+    target.mkdirs()
+
+    log.info(s"JFlex: Using JFlex version ${Main.version} to generate source files.")
+    Options.dot = tool.dot
+    Options.verbose = tool.verbose
+    Options.dump = tool.dump
+    Options.setDir(target.getPath)
+    Options.emitScala = tool.emitScala
+
+    val grammars = (srcDir ** ("*" + options.grammarSuffix)).get
+    log.info(s"JFlex: Generating source files for ${grammars.size} grammars.")
+
+    grammars.foreach { g =>
+      Main.generate(g)
+      log.info(s"JFlex: Grammar file ${g.getPath} detected.")
     }
   }
+
 
 }
