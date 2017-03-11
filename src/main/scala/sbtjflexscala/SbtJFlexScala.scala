7aa7e358a0df0cacf1689b9be48525fefb2f6558
Yusuke Izawa <yuizalp@gmail.com>
2017-03-11 17:16:12 +0900

    remove generateWithCompile option and fix minor

--- src/main/scala/sbtjflexscala/SbtJFlexScala.scala
+++ src/main/scala/sbtjflexscala/SbtJFlexScala.scala
@@ -5,6 +5,8 @@ import Keys._
 
 import jflex.{ Options, Main }
 
+import scala.util.{ Try, Success, Failure }
+
 object SbtJFlexScala extends AutoPlugin {
 
   case class JFlexToolConfiguration(
@@ -19,13 +21,13 @@ object SbtJFlexScala extends AutoPlugin {
   )
 
   object autoImport {
-    lazy val jflexSourceDirectory: SettingKey[File] = settingKey[File]("jflex-source-directory")
-    lazy val jflexOutputDirectory: SettingKey[File] = settingKey[File]("jflex-output-directory")
-    lazy val toolConfiguration: SettingKey[JFlexToolConfiguration] = settingKey[JFlexToolConfiguration]("jflex-tool-configuration")
-    lazy val pluginConfiguration: SettingKey[PluginConfiguration] = settingKey[PluginConfiguration]("jflex-plugin-configuration")
-    lazy val jflexGenerateWithCompile: SettingKey[Boolean] = settingKey[Boolean]("jflex-with-compile")
-    lazy val jflexSources: TaskKey[Seq[File]] = taskKey[Seq[File]]("jflex-sources")
-    lazy val jflexGenerate: TaskKey[Unit] = taskKey[Unit]("jflex-generate")
+    lazy val jflex = taskKey[Unit]("jflex")
+    lazy val sourceDirectory = settingKey[File]("jflex-source-directory")
+    lazy val outputDirectory = settingKey[File]("jflex-output-directory")
+    lazy val toolConfiguration = settingKey[JFlexToolConfiguration]("jflex-tool-configuration")
+    lazy val pluginConfiguration = settingKey[PluginConfiguration]("jflex-plugin-configuration")
+    lazy val jflexSources = taskKey[Seq[File]]("jflex-sources")
+    lazy val generate = taskKey[Unit]("jflex-generate")
   }
 
   import autoImport._
@@ -33,27 +35,19 @@ object SbtJFlexScala extends AutoPlugin {
   override def trigger: PluginTrigger = allRequirements
 
   override val projectSettings: Seq[Setting[_]] = Seq(
-    jflexSourceDirectory := sourceDirectory.value / "main" / "flex",
-    jflexOutputDirectory := sourceDirectory.value / "main" / "scala" / "flex",
+    sourceDirectory in jflex := sourceDirectory.value / "main" / "flex",
+    outputDirectory in jflex := sourceDirectory.value / "main" / "scala" /  "flex",
     toolConfiguration := JFlexToolConfiguration(),
     pluginConfiguration := PluginConfiguration(),
-    jflexSources := (jflexSourceDirectory.value ** "*.flex").get,
-    jflexGenerate := jflexGeneratorTask.value,
-    unmanagedSourceDirectories in Compile += jflexSourceDirectory.value,
-    jflexGenerateWithCompile := false
-  ) ++ Seq(
-    compile := {
-      if (jflexGenerateWithCompile.value)
-        (compile in Compile).dependsOn(jflexGenerate).value
-      else
-        (compile in Compile).value
-      }
+    jflexSources := (sourceDirectory.value ** "*.flex").get,
+    generate := jflexGeneratorTask.value,
+    unmanagedSourceDirectories in Compile += (sourceDirectory in jflex).value,
   )
 
   lazy val jflexGeneratorTask: Def.Initialize[Task[Unit]] = Def.task {
     generateWithJFlex(
-      jflexSources.value,
-      jflexOutputDirectory.value,
+      (jflexSources in jflex).value,
+      (outputDirectory in jflex).value,
       toolConfiguration.value,
       pluginConfiguration.value,
       streams.value.log
